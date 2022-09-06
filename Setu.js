3b0ca1a104cd155a11f405aea291327886ab9fbb
RGB <79316659+reallyGoodBaker@users.noreply.github.com>
2022-09-06 22:54:37 +0800

    Update Setu.js

--- Setu.js
+++ Setu.js
@@ -1,8 +1,42 @@
+const interfaces = require('../interfaces.js');
+const { Plugin, Schedule } = require('../plugin.js');
+const Service = require('../service.js');
 /// <reference path="extension.d.ts"/>
 const request = require('request');
 const { segment } = require("oicq")
 
-const SetuUrl = 'https://api.lolicon.app/setu/v2?r18=1&'
+const SetuUrl = 'https://api.lolicon.app/setu/v2?r18=1&size=original&size=thumb&'
+
+const jsonCard = {
+    "app": "com.tencent.structmsg",
+    "desc": "{{desc}}",
+    "view": "news",
+    "ver": "0.0.0.1",
+    "prompt": "[分享]湖南工程学院2022年秋季学期开学须知！",
+    "meta": {
+        "news": {
+            "action": "",
+            "android_pkg_name": "",
+            "app_type": 1,
+            "appid": 1,
+            "ctime": 1662473381411,
+            "desc": "desc",
+            "jumpUrl": "https:\/\/www.baidu.com\/favicon.ico",
+            "preview": "https:\/\/www.baidu.com",
+            "source_icon": "https:\/\/www.baidu.com\/favicon.ico",
+            "source_url": "",
+            "tag": "tag",
+            "title": "title",
+            "uin": 1323995382
+        }
+    },
+    "config": {
+        "ctime": 1662473381411,
+        "forward": true,
+        "token": "46c4a5ee7fed552441e044df12455a8a",
+        "type": "normal"
+    }
+}
 
 class Setu extends Plugin {
 
@@ -17,16 +51,28 @@ class Setu extends Plugin {
      * @param {InitBundle} bundle
      */
     init(bundle) {
-        const {bot, asyncTask, args} = bundle
-        const [type, count] = args
+        const { bot, asyncTask, args } = bundle
+        let count = args[args.length - 1]
+
+        if (args.length === 1 && !isNaN(+count)) {
+            return asyncTask(bot.send(`无效的参数`))
+        }
+
+        if (isNaN(+count)) {
+            count = 1
+        } else {
+            args.pop()
+        }
+
+        const type = args.map(v => `tag=${encodeURI(v)}`).join('&')
 
         asyncTask(resolve => {
-            request.get(`${SetuUrl}tag=${encodeURI(type)}&num=${count > 0 && count < 21 ? count: 1}`, (err, res, body) => {
+            request.get(`${SetuUrl}${type}&num=${count > 0 && count < 21 ? count : 1}`, (err, res, body) => {
                 if (err) {
                     asyncTask(bot.send(`请求失败\n${err}`))
                     return resolve()
                 }
-    
+
                 let data = null
                 try {
                     data = JSON.parse(body)
@@ -34,26 +80,35 @@ class Setu extends Plugin {
                     asyncTask(bot.send(`数据解析失败\n${err}`))
                     return resolve()
                 }
-    
+
                 if (data.error) {
                     asyncTask(bot.send(`获取失败\n${data.error}`))
                     return resolve()
                 }
-    
+
                 let msg = []
                 data.data.forEach(setu => {
-                    const {pid, title, author, urls} = setu
-                    msg.push(`${title} - ${author}`)
-                    msg.push(segment.image(urls.thumb))
+                    const { pid, title, author, urls } = setu
+                    msg.push(`[${pid}] ${title} - ${author}\n${urls.original}`)
+                    if (!count || count === 1) {
+                        let sender = null
+                        asyncTask(sender = bot.send(segment.image(urls.thumb)))
+
+                        sender.then(v => {
+                            setTimeout(() => {
+                                bot.recall(v?.data?.message_id)
+                            }, 5000)
+                        })
+                    }
                 })
-    
-                asyncTask(bot.send(msg))
+
+                asyncTask(bot.send(msg.join('\n')))
                 resolve()
             })
         })
     }
 
-    exit() {}
+    exit() { }
 }
 
 
